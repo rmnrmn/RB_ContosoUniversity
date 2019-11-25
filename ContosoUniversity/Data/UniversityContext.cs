5360c3632e4f6f43c4ac5eb9b0ab9a0518c47308
Ramona Balan <21800312@bucks.ac.uk>
2019-11-25 11:27:24 +0000

    Add project files.

---
+++ ContosoUniversity/Data/UniversityContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Models
+{
+    public class UniversityContext : DbContext
+    {
+        public UniversityContext (DbContextOptions<UniversityContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Student { get; set; }
+        public DbSet<Enrollment> Enrollment { get; set; }
+        public DbSet<Course> Course { get; set; }
+    }
+}
---
+++ ContosoUniversity/Pages/Details.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Pages
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ContosoUniversity.Models.UniversityContext _context;
+
+        public DetailsModel(ContosoUniversity.Models.UniversityContext context)
+        {
+            _context = context;
+        }
+
+        public Student Student { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Student = await _context.Student.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Student == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
